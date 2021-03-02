17f451f29725452973cf932f9db33c930e7c6c8e
seankuendig <18509274+seankuendig@users.noreply.github.com>
2021-03-02 12:53:27 +0100

    implement discord configuration

---
+++ json_handler.py
@@ -0,0 +1,24 @@
+import json
+
+
+def update(config_param):
+    with open('config.json') as json_file:
+        data = json.load(json_file)
+        print(data)
+        config_already_written = False
+        for config in data['configs']:
+            print(config)
+            if config_param['user_id'] == config['user_id']:
+                config['diet'] = config_param['diet']
+                config['exclude'] = config_param['exclude']
+                config['target_calories'] = config_param['target_calories']
+                config_already_written = True
+        if not config_already_written:
+            data['configs'].append(config_param)
+        print(data)
+        dump_data(data)
+
+
+def dump_data(data):
+    with open('config.json', 'w') as json_file:
+        json.dump(data, json_file)
